bf442b3e95d6e52d612545f58536ef1e64d51e07
Danish <danishasifdev@gmail.com>
2024-07-12 15:26:37 +0500

    fix:fix-errors

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ import Footer from "./components/Footer"
 
 export default function Home() {
   return (
+    // Check
     <main className="flex-auto bg-[#120e1b] mb-0">
       <Navbar />
       <TestimonialsTop />
